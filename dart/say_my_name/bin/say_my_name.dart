c571339d29bb49a850de8ab850254c6b4baa64da
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-30 10:03:00 -0300

    [say_my_name] - initial commit

---
+++ dart/say_my_name/bin/say_my_name.dart
@@ -0,0 +1,5 @@
+import 'package:say_my_name/say_my_name.dart' as say_my_name;
+
+void main(List<String> arguments) {
+  print('Hello world: ${say_my_name.calculate()}!');
+}
---
+++ dart/say_my_name/test/say_my_name_test.dart
@@ -0,0 +1,8 @@
+import 'package:say_my_name/say_my_name.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
