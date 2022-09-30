a5a50e23895f2f829375883ff06e778cd276e455
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-30 10:27:48 -0300

    [tell_my_age] initial commit

---
+++ dart/tell_my_age/bin/tell_my_age.dart
@@ -0,0 +1,5 @@
+import 'package:tell_my_age/tell_my_age.dart' as tell_my_age;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tell_my_age.calculate()}!');
+}
---
+++ dart/tell_my_age/test/tell_my_age_test.dart
@@ -0,0 +1,8 @@
+import 'package:tell_my_age/tell_my_age.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
