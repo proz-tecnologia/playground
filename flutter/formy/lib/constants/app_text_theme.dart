ac4bddc09f7688ac73f23a20166bfe83b2c543f7
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-05 21:43:05 -0300

    [feat]: add custom colors and text theme

---
+++ flutter/formy/lib/constants/app_text_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTextTheme {
+  AppTextTheme._();
+  static const TextStyle caption = TextStyle(
+    color: AppColors.white,
+    fontFamily: 'Quicksand',
+    fontWeight: FontWeight.w700,
+    fontSize: 48,
+  );
+}
