2fde430d6f3a8b354408c57a8630afb63b49886e
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-14 12:55:54 -0300

    [LISTY] remove arquivos desnessarios

--- dart/listy/bin/listy.dart
+++ dart/listy/bin/listy.dart
@@ -1,5 +1,1 @@
-import 'package:listy/listy.dart' as listy;
-
-void main(List<String> arguments) {
-  print('Hello world: ${listy.calculate()}!');
-}
+void main() {}
--- dart/listy/test/listy_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:listy/listy.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
